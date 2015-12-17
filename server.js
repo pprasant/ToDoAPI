a019c33148f58eec0284d18d74f19cedefa654f6
Prashant Singh Thakuri <daprasan@outlook.com>
2015-12-17 18:07:31 +1100

    add PUT route

--- server.js
+++ server.js
@@ -78,6 +78,39 @@ app.delete('/todos/:id', function (req, res){
 	
 });
 
+
+// PUT /todos/:id
+
+app.put('/todos/:id', function(req, res){
+	
+	var todoId = parseInt(req.params.id);
+	var matchedToDo = _.findWhere(todos, {id: todoId});
+	
+	if(!matchedToDo){
+		return res.status(404).send();
+	}
+	
+	var body = _.pick(req.body, 'description', 'completed');
+	var validAttributes = {};
+	
+	
+	if(body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
+		validAttributes.completed = body.completed;
+	} else if(body.hasOwnProperty('completed')) {
+		return res.status(400).send();
+	}
+	
+	if(body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0){
+		validAttributes.description = body.description;
+	} else if(body.hasOwnProperty('description')) {
+		res.status(400).send();
+	}
+	
+	_.extend(matchedToDo, validAttributes);
+	res.json(matchedToDo);
+	
+});
+
 app.listen(PORT, function(){
 	console.log('Express listening on port ' + PORT);
 });
